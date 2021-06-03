84292ee1c9165393ee1611f350d3dc065753b4fc
AmandineGrosfils <62287195+AmandineGrosfils@users.noreply.github.com>
2021-06-03 11:07:27 +0200

    Delete dose model scripts

--- Dose-model/DoseModelPredict.m
+++
@@ -1,6 +0,0 @@
-function [ModelPrediction,Dieff]=DoseModelPredict(D,fi,aij)
-%%
-%% Calculate the effetive dose
-Dieff=calc_effective_dose(D,fi,aij);
-%% Calculate the predicted response
-ModelPrediction=BlissPredict(Dieff,fi);
--- Dose-model/create_folders_for_figs.m
+++
@@ -1,6 +0,0 @@
-function create_folders_for_figs(ExpName)
-mkdir(ExpName);
-mkdir([ExpName '/fig/']);
-mkdir([ExpName '/pdf/']);
-%mkdir([ExpName '/emf/']);
-mkdir([ExpName '/png/']);
